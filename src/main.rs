886c6d45db0b4b40e1300471e7528a57f0998757
pinchao <cpc4253@163.com>
2023-07-20 16:27:44 +0800

    some simple examples

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+#[allow(warnings)]
+
 fn main() {
     println!("Hello, world!");
+
+    let condition = false;
+    let some_number = 5;
+    let string_number = "5";
+    let names = ["Eric", "Nancy", "Lily"];
+    let number = string_number.parse::<i32>().unwrap();
 }
